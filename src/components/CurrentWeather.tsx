688f8b8783dccdfc22264d7956342e9c31e8286b
teddy.nutech <teddy.nutech@gmail.com>
2024-02-22 15:25:52 +0700

    update

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import moment from "moment";
 import { useContext, useEffect, useState } from "react";
 import * as context from "../helper/Context";
 
@@ -7,14 +8,15 @@ const CurrentWeather = () => {
   let [data, setData]: any = useState(null);
   const [time, setTime]: any = useState(null);
   const [image, setImage]: any = useState(null);
+  const [sun, setSun]: any = useState({});
 
   const getCurrentWeather = async () => {
     if (location.latitude !== undefined) {
       await axios
         .get(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=73d50daa62ff9ab6a412d4f16d0d6499&units=metric&mode=json&lang=id`
+          `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=73d50daa62ff9ab6a412d4f16d0d6499&units=metric&mode=json`
         )
-        .then((data) => {
+        .then(async (data) => {
           console.log(data.data);
           setData(data.data);
           setImage(
@@ -22,6 +24,7 @@ const CurrentWeather = () => {
               data.data.weather[0].icon +
               "@4x.png"
           );
+          // await getState(data.data.name);
           // return (result = data.data);
         })
         .catch((err) => {
@@ -30,12 +33,24 @@ const CurrentWeather = () => {
     }
   };
 
+  const getState = async (city: string) => {
+    await axios
+      .get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=2&appid=73d50daa62ff9ab6a412d4f16d0d6499`
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
   useEffect(() => {
     getCurrentWeather();
     let date = new Date();
     setTime(date.toLocaleString());
+    let sunrise = moment(1708600473).format("HH:mm:ss");
+    setSun(sunrise);
   }, [location]);
-
+  console.log(sun);
   return (
     <>
       <div className="flex flex-col border border-zinc-100 w-full p-2 space-y-2 bg-white rounded-md">
@@ -54,7 +69,7 @@ const CurrentWeather = () => {
                     src={image}
                     alt="icon"
                   ></img>
-                  <span>{data?.weather[0].main}</span>
+                  <span>{data?.weather[0].description}</span>
                 </span>
                 <span className="flex flex-col text-xl">
                   <span>{parseInt(data?.main.temp)}°C</span>
@@ -66,39 +81,6 @@ const CurrentWeather = () => {
                 <span>{data?.name}</span>
               </span>
             </div>
-
-            {/* <span className="flex justify-center flex-col space-y-4">
-            <span className="flex justify-center flex-col max-sm:text-sm text-lg">
-              <span className="flex justify-center">Humidity</span>
-              <span className="flex justify-center">
-                {data?.main.humidity}%
-              </span>
-            </span>
-            <span className="flex justify-center flex-col max-sm:text-sm text-lg">
-              <span className="flex">Pressure</span>
-              <span className="flex justify-center">
-                {data?.main.pressure} mb
-              </span>
-            </span>
-          </span>
-          <span className="flex justify-center flex-col space-y-4">
-            <span className="flex flex-col max-sm:text-sm text-lg">
-              <span className="flex justify-center">Wind Speed</span>
-              <span className="flex justify-center">
-                {data?.wind.speed} m/s
-              </span>
-            </span>
-            <span className="flex flex-col max-sm:text-sm text-lg">
-              <span className="flex justify-center">Visibility</span>
-              <span className="flex justify-center">
-                {data?.visibility / 1000} km
-              </span>
-            </span>
-          </span> */}
-            {/* <span className="flex">
-            <span className="flex justify-between"></span>
-          </span>
-          <span className="flex flex-wrap"></span> */}
           </div>
           <div className="flex flex-row max-sm:flex-wrap max-sm:space-y-5 max-sm:space-x-0 space-x-5  w-full p-2">
             <div className="flex flex-col items-center space-y-3 text-2xl max-sm:text-base w-full justify-center">
@@ -137,6 +119,13 @@ const CurrentWeather = () => {
                   <i className="fa-solid fa-temperature-arrow-up text-xl max-sm:text-base"></i>
                 </span>
                 <span>{Math.round(data?.main.temp_max)}°C</span>
+              </div>
+              <div className="flex max-sm:w-full w-1/2 justify-between border-b-4">
+                <span>
+                  Min Temperature{" "}
+                  <i className="fa-solid fa-temperature-arrow-down text-xl max-sm:text-base"></i>
+                </span>
+                <span>{Math.round(data?.main.temp_min)}°C</span>
               </div>
             </div>
           </div>
